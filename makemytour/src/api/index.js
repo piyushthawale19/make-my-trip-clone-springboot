16eb3a8365fa777d6ef45b6878a00b7e30ca02b2
PIYUSH ARUN THAWLE <151814546+piyushthawale19@users.noreply.github.com>
2025-06-14 21:38:30 +0530

    Update index.js
    update backend

--- makemytour/src/api/index.js
+++ makemytour/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8080";
+const BACKEND_URL = "https://make-my-trip-clone-springboot-1-wbv2.onrender.com";
 
 export const login = async (email, password) => {
   try {
